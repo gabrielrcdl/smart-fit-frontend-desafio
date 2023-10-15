3a9c1076f259da7e5eb975ebdeac5de9d959a180
gr@email.com <gr78787@gmail.com>
2023-10-15 15:02:14 -0300

    feat: add form

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+  results = [];
+  form!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  onClear() {
+    this.form.reset();
+    console.log(this.form.value);
+  }
 }
